6107a0ca6a37eaa070c449fd1e27992f42023f24
vickypaiyaa <vickypaiyaa@gmail.com>
2020-03-27 23:16:45 +0530

    chat window files added

--- app/public/js/chat.js
+++ app/public/js/chat.js
@@ -1,37 +1,65 @@
-var socket = io();
-var chatUsername = document.querySelector('#chat-username');
-var chatMessage = document.querySelector('#chat-message');
 
-socket.on('connect', function() {
-  var chatForm = document.forms.chatForm;
+var charForm = document.forms.chatform;
 
-  if (chatForm) {
-    chatForm.addEventListener('submit', function(e) {
-      e.preventDefault();
-      socket.emit('postMessage',{
-        username: chatUsername.value,
-        message: chatMessage.value,
-      });
-      chatMessage.value='';
-      chatMessage.focus();
-    }); //chatform event
+if (chatForm) {
+  var chatUsername = document.querySelector('#chat-username');
+  var chatMessage = document.querySelector('#chat-message');
 
-    socket.on('updateMessages', function(data) {
-      showMessage(data);
-    }); //updateMessages
-  } //chatform
-}); //socket
+  chatForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    showMessage({
+      username : chatUsername.value,
+      message : chatMessage.value,
+    });
+    chatMessage.value = '';
+    chatMessage.focus();
+  });
+}
 
 function showMessage(data) {
   var chatDisplay = document.querySelector('.chat-display');
   var newMessage = document.createElement('p');
-
-  if (chatUsername.value == data.username) {
-    newMessage.className = 'bg-success chat-text';
-  } else {
-    newMessage.className = 'bg-info text-warning chat-text';
-  }
-
-  newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
+  newMessage.className = 'bg-successs chat-text';
+  newMessage.innerHTML= '<strong>' + data.username + ' </strong>: ' + data.message;
   chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
-}
+} 
+
+
+
+// var socket = io();
+// var chatUsername = document.querySelector('#chat-username');
+// var chatMessage = document.querySelector('#chat-message');
+
+// socket.on('connect', function() {
+//   var chatForm = document.forms.chatForm;
+
+//   if (chatForm) {
+//     chatForm.addEventListener('submit', function(e) {
+//       e.preventDefault();
+//       socket.emit('postMessage',{
+//         username: chatUsername.value,
+//         message: chatMessage.value,
+//       });
+//       chatMessage.value='';
+//       chatMessage.focus();
+//     }); //chatform event
+
+//     socket.on('updateMessages', function(data) {
+//       showMessage(data);
+//     }); //updateMessages
+//   } //chatform
+// }); //socket
+
+// function showMessage(data) {
+//   var chatDisplay = document.querySelector('.chat-display');
+//   var newMessage = document.createElement('p');
+
+//   if (chatUsername.value == data.username) {
+//     newMessage.className = 'bg-success chat-text';
+//   } else {
+//     newMessage.className = 'bg-info text-warning chat-text';
+//   }
+
+//   newMessage.innerHTML = '<strong>' + data.username + '</strong>: ' + data.message;
+//   chatDisplay.insertBefore(newMessage, chatDisplay.firstChild);
+// }
